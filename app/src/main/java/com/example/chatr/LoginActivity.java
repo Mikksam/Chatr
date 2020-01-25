be6f8c7fdd3847555001f6c1c12eb273423dbe24
mikksam <samuel.mikkonen@edu.savonia.fi>
2020-01-25 12:51:09 +0200

    Commented login and mainActivity codes and renamed icons

--- app/src/main/java/com/example/chatr/LoginActivity.java
+++ app/src/main/java/com/example/chatr/LoginActivity.java
@@ -4,19 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseUser currentUser;
+    private Button loginButton, phoneLoginButton;
+    private EditText usrEmail, usrPassword;
+    private TextView forgotPasswordLink, createAccountLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        InitializeComponents();
+
+        //Create-account-listener that activates on click and sends user to register Activity
+        createAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SendUserToRegisterActivity();
+            }
+        });
     }
 
+    //Initialize components: Buttons, TextViews and EditText fields
+    private void InitializeComponents() {
+
+        loginButton = (Button) findViewById(R.id.login_button);
+        phoneLoginButton = (Button) findViewById(R.id.phone_login_button);
+        usrEmail = (EditText) findViewById(R.id.login_email);
+        usrPassword = (EditText) findViewById(R.id.login_password);
+        createAccountLink = (TextView) findViewById(R.id.create_account_login_link);
+        forgotPasswordLink = (TextView) findViewById(R.id.forgot_password);
+    }
+
+    //Check if already logged in
     @Override
     protected void onStart() {
         super.onStart();
@@ -31,5 +60,11 @@ public class LoginActivity extends AppCompatActivity {
     private void SendUserToMainActivity() {
         Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(loginIntent);
+    }
+
+    //Send user to register Activity
+    private void SendUserToRegisterActivity() {
+        Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(registerIntent);
     }
 }
