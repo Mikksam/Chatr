5d44cffcc3263d25ffd9ff226686e5d1af536220
mikksam <samuel.mikkonen@edu.savonia.fi>
2020-01-27 18:10:27 +0200

    Made phone verification layout

--- app/src/main/java/com/example/chatr/PhoneLoginActivity.java
+++ app/src/main/java/com/example/chatr/PhoneLoginActivity.java
@@ -3,6 +3,9 @@ package com.example.chatr;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
@@ -13,12 +16,35 @@ public class PhoneLoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private DatabaseReference PhoneLogRef;
 
+    private EditText phoneNumber, verificationCode;
+    private Button phoneNumberButton, verifyButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_login);
 
         PhoneLogRef = FirebaseDatabase.getInstance().getReference();
+
+        InitializeComponents();
+
+        phoneNumberButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+    //Initialize components used in the phone login layout
+    private void InitializeComponents() {
+
+        phoneNumber = (EditText) findViewById(R.id.phone_number);
+        verificationCode = (EditText) findViewById(R.id.phone_verification_number);
+        phoneNumberButton = (Button) findViewById(R.id.phone_submit_button);
+        verifyButton = (Button) findViewById(R.id.verify_button);
+
     }
 
 
