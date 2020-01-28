f5c5d64665b1321bf1d7efa04225f856430f0dc6
mikksam <samuel.mikkonen@edu.savonia.fi>
2020-01-28 17:33:13 +0200

    Visibility changes when button pressed and check if phone nbr empty

--- app/src/main/java/com/example/chatr/PhoneLoginActivity.java
+++ app/src/main/java/com/example/chatr/PhoneLoginActivity.java
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -17,6 +22,7 @@ public class PhoneLoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private DatabaseReference PhoneLogRef;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
 
     private EditText phoneNumber, verificationCode;
     private Button phoneNumberButton, verifyButton;
@@ -31,13 +37,39 @@ public class PhoneLoginActivity extends AppCompatActivity {
 
         InitializeComponents();
 
-        //Sends phone number to Firebase
+
+        //Phone number check and authorization with firebase
+        // Documentation in -> https://firebase.google.com/docs/auth/android/phone-auth
         phoneNumberButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Check if there is a phone number
+                if(TextUtils.isEmpty(phoneNumber.getText())){
+                    Toast.makeText(PhoneLoginActivity.this, "Please enter your phone number...", Toast.LENGTH_SHORT).show();
+                }
+                else{
 
+                    //Make phone number fields invisible so that cannot send again
+                    phoneNumberButton.setVisibility(View.INVISIBLE);
+                    phoneNumber.setVisibility(View.INVISIBLE);
+
+                    //Make verification fields visible
+                    verificationCode.setVisibility(View.VISIBLE);
+                    verifyButton.setVisibility(View.VISIBLE);
+
+                   /*
+                   //For some reason doesn't understand TimeUnit and explodes when imported java-library for it
+                    PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                            phoneNumber,        // Phone number to verify
+                            60,                 // Timeout duration
+                            TimeUnit.SECONDS,   // Unit of timeout
+                            PhoneLoginActivity.this,               // Activity (for callback binding)
+                            mCallbacks);        // OnVerificationStateChangedCallbacks
+                    */
+                }
             }
         });
+
 
         //Calls for method that sends user back to login activity
         goBack.setOnClickListener(new View.OnClickListener() {
